2a9941293117e3489d2f010a96c98eedd0bf8041
Herbert Kipkoech <105177885+lagathub@users.noreply.github.com>
2025-11-29 06:51:21 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -158,9 +158,7 @@ const CONFIG = {
     username: 'arifszn', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
+//--analytics was here
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
